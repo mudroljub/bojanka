03f642832beebbfef5adc8513c5c658286fe759e
Damjan Pavlica <mudroljub@gmail.com>
2017-02-09 12:46:51 +0100

    velicina prozora

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow} = require('electron')
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({
-    width: 600,
+    width: 1200,
     height: 800
   })
   mainWindow.loadURL('file://' + __dirname + '/index.html')
